d2f1be37ff2cae578636d48927cb168cf2a225dc
Cal <callum.adair@outlook.com>
2023-02-23 22:16:30 +0000

    rerfixed the silly change in array value sizes.

--- src/main.rs
+++ src/main.rs
@@ -31,16 +31,18 @@ fn calculate_4yr() {
     println!("Overall grade for four-year advanced degree is: {}.", overall_grade);
 }
 
-fn fill_4yr() -> ([u8; 8], [u8; 8], [u8; 8]) {
-    let mut fourth_yr_arr: [u8; 8] = [0; 8];
+fn fill_4yr() -> ([u16; 8], [u16; 8], [u16; 8]) {
+    println!("Enter all grades for a given year on a single line delimited by spaces.");
+
+    let mut fourth_yr_arr: [u16; 8] = [0; 8];
     println!("Please enter your module grades for fourth year.");
     fill_array(&mut fourth_yr_arr);
 
-    let mut third_yr_arr: [u8; 8] = [0; 8];
+    let mut third_yr_arr: [u16; 8] = [0; 8];
     println!("Please enter your module grades for third year.");
     fill_array(&mut third_yr_arr);
 
-    let mut second_yr_arr: [u8; 8] = [0; 8];
+    let mut second_yr_arr: [u16; 8] = [0; 8];
     println!("Please enter your module grades for second year.");
     fill_array(&mut second_yr_arr);
 
@@ -48,12 +50,13 @@ fn fill_4yr() -> ([u8; 8], [u8; 8], [u8; 8]) {
 }
 
 fn calculate_3yr() {
-    let mut third_yr_arr: [u8; 8] = [0; 8];
+    println!("Enter all grades for a given year on a single line delimited by spaces.");
+    let mut third_yr_arr: [u16; 8] = [0; 8];
     println!("Please enter your module grades for third year.");
     fill_array(&mut third_yr_arr);
 
-    let mut second_yr_arr: [u8; 8] = [0; 8];
-    println!("Please enter your module grades for second year.");
+    let mut second_yr_arr: [u16; 8] = [0; 8];
+    println!("Please enter your module grades for second year on.");
     fill_array(&mut second_yr_arr);
 
     let mut array_tuple = (third_yr_arr, second_yr_arr);
@@ -62,7 +65,7 @@ fn calculate_3yr() {
     println!("Overall grade for three-year standard degree is: {}.", overall_grade);
 }
 
-fn fill_array(arr: &mut [u8; 8]) -> &mut [u8; 8] {
+fn fill_array(arr: &mut [u16; 8]) -> &mut [u16; 8] {
     let mut input_line = String::new();
     io::stdin()
         .read_line(&mut input_line)
@@ -80,7 +83,7 @@ fn fill_array(arr: &mut [u8; 8]) -> &mut [u8; 8] {
     return arr;
 }
 
-fn weight_and_average_4yr(grade_array_tuple: &mut ([u8; 8], [u8; 8], [u8; 8])) -> f32 {
+fn weight_and_average_4yr(grade_array_tuple: &mut ([u16; 8], [u16; 8], [u16; 8])) -> f32 {
     let mut sum: f32 = 0.0;
     let total_weight: f32 = 60.0;
 
@@ -108,7 +111,7 @@ fn weight_and_average_4yr(grade_array_tuple: &mut ([u8; 8], [u8; 8], [u8; 8])) -
     return sum / total_weight;
 }
 
-fn weight_and_average_3yr(grade_array_tuple: &mut ([u8; 8], [u8; 8])) -> f32 {
+fn weight_and_average_3yr(grade_array_tuple: &mut ([u16; 8], [u16; 8])) -> f32 {
     let mut sum: f32 = 0.0;
     let total_weight: f32 = 33.0;
 
